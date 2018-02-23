cbfcee55c44ce173a7d84338e606770f81e36fc4
Gennady Myasnikov <job.myasnikov.gennady@gmail.com>
2018-02-23 13:16:23 +0300

    no message

---
+++ MyInvoice/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MyInvoice.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ MyInvoice/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MyInvoice.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
