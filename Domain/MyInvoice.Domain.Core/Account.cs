2f54effb95787de311e8ae358b5a5cb00a186c67
Gennady Myasnikov <job.myasnikov.gennady@gmail.com>
2018-02-27 14:40:08 +0300

    minor changes

--- Domain/MyInvoice.Domain.Core/Account.cs
+++ Domain/MyInvoice.Domain.Core/Account.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyInvoice.Domain.Core
-{
-    public class Account
-    {
+﻿namespace MyInvoice.Domain.Core {
+    public class Account {
         public int Id { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
