63ebfde6824a499abd875b6c40c39ece20706401
Gennady Myasnikov <job.myasnikov.gennady@gmail.com>
2018-03-12 18:36:05 +0300

    Property model added

---
+++ Domain/MyInvoice.Domain.Core/PropertyCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyInvoice.Domain.Core
+{
+    public class PropertyCategory
+    {
+        public int Id { get; set; }
+        public int PropertyId { get; set; }
+        public string CategoryName { get; set; }
+
+        public virtual Property Property { get; set; }
+    }
+}
---
+++ Domain/MyInvoice.Domain.Core/PropertyType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyInvoice.Domain.Core
+{
+    public class Property
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string ValidationExpression { get; set; }
+        public string StringFormat { get; set; }
+
+        public virtual ICollection<PropertyCategory> PropertyCategories { get; set; }
+    }
+}
